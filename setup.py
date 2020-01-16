99adf78bc740ac910d601ffb892703fcddf54140
larrywax <giuseppe.danna@prima.it>
2020-01-16 10:39:40 +0100

    bump version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name="autoupgrade-prima",
-    version="0.3.3",
+    version="0.4.0",
     author="Walter Purcaro",
     author_email="vuolter@gmail.com",
     description="Automatic upgrade of PyPI packages",
